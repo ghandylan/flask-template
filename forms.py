a95dc65acf6446ec295b8c4747a3816c79fab2ef
Dylan Louis Tayag <80078209+ghandylan@users.noreply.github.com>
2023-11-06 20:34:00 +0800

    fixed max email and pw length

--- forms.py
+++ forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import DataRequired, Length, EqualTo
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=50)])
+    email = EmailField('Email', validators=[DataRequired(), Length(min=3, max=255)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=64)])
 
 
 class RegisterForm(FlaskForm):
@@ -14,7 +14,7 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Password',
                              validators=[
                                  DataRequired(),
-                                 Length(min=8, max=50),
+                                 Length(min=8, max=64),
                                  EqualTo('confirm_password', message='Passwords must match')
                              ])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), Length(min=8)])
